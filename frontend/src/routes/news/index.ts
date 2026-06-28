738cd2cd540d3d589a9ee45c5a3280341f045c5f
ghattanCrazyKiller <ghattanilhaq@gmail.com>
2026-06-28 20:33:30 +0700

    ntah

--- frontend/src/routes/news/index.ts
+++ frontend/src/routes/news/index.ts
@@ -33,68 +33,68 @@ store.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
     method: 'post',
 })
 
-    /**
+/**
 * @see \App\Http\Controllers\Api\NewsController::store
- * @see app/Http/Controllers/Api/NewsController.php:19
- * @route '/api/admin/news'
- */
-    const storeForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-        action: store.url(options),
-        method: 'post',
-    })
+* @see app/Http/Controllers/Api/NewsController.php:19
+* @route '/api/admin/news'
+*/
+const storeForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: store.url(options),
+    method: 'post',
+})
 
-            /**
+/**
 * @see \App\Http\Controllers\Api\NewsController::store
- * @see app/Http/Controllers/Api/NewsController.php:19
- * @route '/api/admin/news'
- */
-        storeForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-            action: store.url(options),
-            method: 'post',
-        })
-    
-    store.form = storeForm
+* @see app/Http/Controllers/Api/NewsController.php:19
+* @route '/api/admin/news'
+*/
+storeForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: store.url(options),
+    method: 'post',
+})
+
+store.form = storeForm
 /**
 * @see \App\Http\Controllers\Api\NewsController::update
  * @see app/Http/Controllers/Api/NewsController.php:42
  * @route '/api/admin/news/{news}'
  */
-export const update = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
+export const update = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
     url: update.url(args, options),
     method: 'put',
 })
 
 update.definition = {
-    methods: ["put","patch"],
+    methods: ["put", "patch"],
     url: '/api/admin/news/{news}',
-} satisfies RouteDefinition<["put","patch"]>
+} satisfies RouteDefinition<["put", "patch"]>
 
 /**
 * @see \App\Http\Controllers\Api\NewsController::update
  * @see app/Http/Controllers/Api/NewsController.php:42
  * @route '/api/admin/news/{news}'
  */
-update.url = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions) => {
+update.url = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions) => {
     if (typeof args === 'string' || typeof args === 'number') {
         args = { news: args }
     }
 
-    
+
     if (Array.isArray(args)) {
         args = {
-                    news: args[0],
-                }
+            news: args[0],
+        }
     }
 
     args = applyUrlDefaults(args)
 
     const parsedArgs = {
-                        news: args.news,
-                }
+        news: args.news,
+    }
 
     return update.definition.url
-            .replace('{news}', parsedArgs.news.toString())
-            .replace(/\/+$/, '') + queryParams(options)
+        .replace('{news}', parsedArgs.news.toString())
+        .replace(/\/+$/, '') + queryParams(options)
 }
 
 /**
@@ -102,7 +102,7 @@ update.url = (args: { news: string | number } | [news: string | number ] | strin
  * @see app/Http/Controllers/Api/NewsController.php:42
  * @route '/api/admin/news/{news}'
  */
-update.put = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
+update.put = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
     url: update.url(args, options),
     method: 'put',
 })
@@ -111,62 +111,62 @@ update.put = (args: { news: string | number } | [news: string | number ] | strin
  * @see app/Http/Controllers/Api/NewsController.php:42
  * @route '/api/admin/news/{news}'
  */
-update.patch = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'patch'> => ({
+update.patch = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteDefinition<'patch'> => ({
     url: update.url(args, options),
     method: 'patch',
 })
 
-    /**
+/**
 * @see \App\Http\Controllers\Api\NewsController::update
- * @see app/Http/Controllers/Api/NewsController.php:42
- * @route '/api/admin/news/{news}'
- */
-    const updateForm = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-        action: update.url(args, {
-                    [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                        _method: 'PUT',
-                        ...(options?.query ?? options?.mergeQuery ?? {}),
-                    }
-                }),
-        method: 'post',
-    })
+* @see app/Http/Controllers/Api/NewsController.php:42
+* @route '/api/admin/news/{news}'
+*/
+const updateForm = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: update.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'PUT',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'post',
+})
 
-            /**
+/**
 * @see \App\Http\Controllers\Api\NewsController::update
- * @see app/Http/Controllers/Api/NewsController.php:42
- * @route '/api/admin/news/{news}'
- */
-        updateForm.put = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-            action: update.url(args, {
-                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                            _method: 'PUT',
-                            ...(options?.query ?? options?.mergeQuery ?? {}),
-                        }
-                    }),
-            method: 'post',
-        })
-            /**
+* @see app/Http/Controllers/Api/NewsController.php:42
+* @route '/api/admin/news/{news}'
+*/
+updateForm.put = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: update.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'PUT',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'post',
+})
+/**
 * @see \App\Http\Controllers\Api\NewsController::update
- * @see app/Http/Controllers/Api/NewsController.php:42
- * @route '/api/admin/news/{news}'
- */
-        updateForm.patch = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-            action: update.url(args, {
-                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                            _method: 'PATCH',
-                            ...(options?.query ?? options?.mergeQuery ?? {}),
-                        }
-                    }),
-            method: 'post',
-        })
-    
-    update.form = updateForm
+* @see app/Http/Controllers/Api/NewsController.php:42
+* @route '/api/admin/news/{news}'
+*/
+updateForm.patch = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: update.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'PATCH',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'post',
+})
+
+update.form = updateForm
 /**
 * @see \App\Http\Controllers\Api\NewsController::destroy
  * @see app/Http/Controllers/Api/NewsController.php:50
  * @route '/api/admin/news/{news}'
  */
-export const destroy = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
+export const destroy = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
     url: destroy.url(args, options),
     method: 'delete',
 })
@@ -181,27 +181,27 @@ destroy.definition = {
  * @see app/Http/Controllers/Api/NewsController.php:50
  * @route '/api/admin/news/{news}'
  */
-destroy.url = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions) => {
+destroy.url = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions) => {
     if (typeof args === 'string' || typeof args === 'number') {
         args = { news: args }
     }
 
-    
+
     if (Array.isArray(args)) {
         args = {
-                    news: args[0],
-                }
+            news: args[0],
+        }
     }
 
     args = applyUrlDefaults(args)
 
     const parsedArgs = {
-                        news: args.news,
-                }
+        news: args.news,
+    }
 
     return destroy.definition.url
-            .replace('{news}', parsedArgs.news.toString())
-            .replace(/\/+$/, '') + queryParams(options)
+        .replace('{news}', parsedArgs.news.toString())
+        .replace(/\/+$/, '') + queryParams(options)
 }
 
 /**
@@ -209,46 +209,46 @@ destroy.url = (args: { news: string | number } | [news: string | number ] | stri
  * @see app/Http/Controllers/Api/NewsController.php:50
  * @route '/api/admin/news/{news}'
  */
-destroy.delete = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
+destroy.delete = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteDefinition<'delete'> => ({
     url: destroy.url(args, options),
     method: 'delete',
 })
 
-    /**
+/**
 * @see \App\Http\Controllers\Api\NewsController::destroy
- * @see app/Http/Controllers/Api/NewsController.php:50
- * @route '/api/admin/news/{news}'
- */
-    const destroyForm = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-        action: destroy.url(args, {
-                    [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                        _method: 'DELETE',
-                        ...(options?.query ?? options?.mergeQuery ?? {}),
-                    }
-                }),
-        method: 'post',
-    })
+* @see app/Http/Controllers/Api/NewsController.php:50
+* @route '/api/admin/news/{news}'
+*/
+const destroyForm = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: destroy.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'DELETE',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'post',
+})
 
-            /**
+/**
 * @see \App\Http\Controllers\Api\NewsController::destroy
- * @see app/Http/Controllers/Api/NewsController.php:50
- * @route '/api/admin/news/{news}'
- */
-        destroyForm.delete = (args: { news: string | number } | [news: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
-            action: destroy.url(args, {
-                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
-                            _method: 'DELETE',
-                            ...(options?.query ?? options?.mergeQuery ?? {}),
-                        }
-                    }),
-            method: 'post',
-        })
-    
-    destroy.form = destroyForm
+* @see app/Http/Controllers/Api/NewsController.php:50
+* @route '/api/admin/news/{news}'
+*/
+destroyForm.delete = (args: { news: string | number } | [news: string | number] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+    action: destroy.url(args, {
+        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+            _method: 'DELETE',
+            ...(options?.query ?? options?.mergeQuery ?? {}),
+        }
+    }),
+    method: 'post',
+})
+
+destroy.form = destroyForm
 const news = {
     store: Object.assign(store, store),
-update: Object.assign(update, update),
-destroy: Object.assign(destroy, destroy),
+    update: Object.assign(update, update),
+    destroy: Object.assign(destroy, destroy),
 }
 
 export default news
