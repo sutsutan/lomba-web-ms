0f0889b94c5c6b44d4e2b5728d73aa32a59c6023
asabrightzzz <hannamariamonika123@gmail.com>
2026-07-10 18:03:44 +0700

    teachers update backend

--- frontend/src/pages/public/Teacher.tsx
+++ frontend/src/pages/public/Teacher.tsx
@@ -143,7 +143,7 @@ const Teachers = () => {
                                             <img
                                                 src={leader.image}
                                                 alt={leader.name}
-                                                className="w-full h-full object-cover grayscale group-hover:grayscale-0 transition-all duration-700"
+                                                className="w-full h-full object-cover transition-all duration-700"
                                             />
                                         ) : (
                                             <Users size={48} className="text-slate-300" />
